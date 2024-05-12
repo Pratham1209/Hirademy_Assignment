bad365c26f09e489a9461cd629cac09634334de7
Pratham1209 <123623948+Pratham1209@users.noreply.github.com>
2024-05-12 23:33:48 +0530

    Add files via upload

---
+++ Assistants/src/main/java/com/Assistants/AssistantsApplication.java
@@ -0,0 +1,14 @@
+package com.Assistants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssistantsApplication.class, args);
+    }
+}
+
+
